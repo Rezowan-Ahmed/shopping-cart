8c49b0ab2a705e94c8865a83af8d9f13c7b34995
Rezowan-Ahmed <ahmedrezowan07@gmail.com>
2020-07-27 07:47:20 +0800

    completed

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-
-function handleProductChange(isIncrease){
-    const caseInput = document.getElementById('case-Count');
-    const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount
+function handleProductChange(product, isIncrease){
+    const productCount = getInputValue(product);
+    let productNewCount = productCount
     if (isIncrease == true) {
-        caseNewCount = caseCount + 1;
+        productNewCount = productCount + 1;
     }
-    if (isIncrease == false && caseCount > 0){
-        caseNewCount = caseCount - 1;
+    if (isIncrease == false && productCount > 0){
+        productNewCount = productCount - 1;
     }
-    caseInput.value = caseNewCount;
-    const caseTotal = caseNewCount * 59;
-    document.getElementById('case-total').innerText = "$" + caseTotal;
+    document.getElementById(product + '-Count').value = productNewCount;
+    let productTotal = 0;
+    if (product == 'phone') {
+        productTotal = productNewCount * 1219;
+    }
+    if (product == 'case') {
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product + '-total').innerText = "$" + productTotal;
+    calculateTotal();
+}
+
+function calculateTotal(){
+    const totalPrice = getInputValue('phone') * 1219 + getInputValue('case') * 59;
+    document.getElementById('total-price').innerText = '$' + totalPrice;
+
+    const tax = Math.round(totalPrice * .05);
+    document.getElementById('tax-amount').innerText = '$' + tax;
+
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
+}
+
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-Count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
 }
